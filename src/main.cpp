d722e08dc3303191f994e81654ec1b2ee631cdeb
Pranav Kumar Kota <pranavkumarkota@gmail.com>
2025-02-20 17:47:01 -0500

    change test

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,58 @@
 #include<iostream>
 
 int main(){
+
 	using tdtype = float;
 	size_t h = 3;
 	size_t w = 3;
 	size_t c = 3;
-	size_t r = 1;
-	size_t s = 1;
+	size_t r = 2;
+	size_t s = 2;
 
-	tensor::Tensor<tdtype> X(2, c, h, w, 'r');
+	// tensor::Tensor<tdtype> X(2, c, h, w, 'r');
 
-	cnn::convLayer cl = cnn::convLayer<tdtype>(2,c,r,s,'s', 'f', 1);
+	cnn::convLayer<tdtype> cl = cnn::convLayer<tdtype>(2,c,r,s,'s', 'f', 1);
 
-	std::cout << "Displaying weights of conv layer (all channels):\n\n";
-	tdtype* weights = cl.getWeights();
-	for(auto i=0; i<r; i++){
-		for(auto j=0; j<s; j++){
-			std::cout << weights[i*s + j] << " ";
-		}
-		std::cout << "\n";
-	}
-	std::cout << "\n";
+	// std::cout << "Displaying weights of conv layer (all channels):\n\n";
+	// tdtype* weights = cl.getWeights();
+	// for(auto i=0; i<r; i++){
+	// 	for(auto j=0; j<s; j++){
+	// 		std::cout << weights[i*s + j] << " ";
+	// 	}
+	// 	std::cout << "\n";
+	// }
+	// std::cout << "\n";
 
 	/////////////////////////////////////////////////////	
 	
-	X.displayShape("X");
-	std::cout << "Displaying input of conv:\n\n";
-	for(auto ch=0; ch<c; ch++){
-		std::cout << "channel: "<<ch<<"\n\t";
-		tdtype *xmat = X.getMatrix(0, ch);
-		for(auto i=0; i<h; i++){
-			for(auto j=0; j<w; j++){
-				std::cout << xmat[i*w + j] << " ";
-			}
-			std::cout << "\n\t";
-		}
-		std::cout << "\n";
-	}
-	std::cout << "\n";
-
+	// X.displayShape("X");
+	// std::cout << "Displaying input of conv:\n\n";
+	// for(auto ch=0; ch<c; ch++){
+	// 	std::cout << "channel: "<<ch<<"\n\t";
+	// 	tdtype *xmat = X.getMatrix(0, ch);
+	// 	for(auto i=0; i<h; i++){
+	// 		for(auto j=0; j<w; j++){
+	// 			std::cout << xmat[i*w + j] << " ";
+	// 		}
+	// 		std::cout << "\n\t";
+	// 	}
+	// 	std::cout << "\n";
+	// }
+	// std::cout << "\n";
+	// X.show("X");
 	/////////////////////////////////////////////////////
-	tensor::Tensor<tdtype> Y = cl.forward(X);
+	// tensor::Tensor<tdtype> Y = cl.forward(X);
 
-	Y.displayShape("Y");
-	std::cout << "Displaying output of conv:\n\n";
-	tdtype *ymat = Y.getMatrix(0, 0);
-	for(auto i=0; i<h; i++){
-		for(auto j=0; j<w; j++){
-			std::cout << ymat[i*w + j] << " ";
-		}
-		std::cout << "\n";
-	}
-	std::cout << "\n";
-    return 0;
+	// Y.displayShape("Y");
+	// std::cout << "Displaying output of conv:\n\n";
+	// tdtype *ymat = Y.getMatrix(0, 0);
+	// for(auto i=0; i<h; i++){
+	// 	for(auto j=0; j<w; j++){
+	// 		std::cout << ymat[i*w + j] << " ";
+	// 	}
+	// 	std::cout << "\n";
+	// }
+	// std::cout << "\n";
+    // return 0;
+	// Y.show("Y");
 }
