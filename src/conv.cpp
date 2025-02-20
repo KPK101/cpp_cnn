39d9f2af59746693206643970aca0d3732ae66a3
Pranav Kumar Kota <pranavkumarkota@gmail.com>
2025-02-20 02:10:16 -0500

    fix conv!

--- src/conv.cpp
+++ src/conv.cpp
@@ -5,17 +5,15 @@ int main(){
 	using tdtype = float;
 	size_t h = 3;
 	size_t w = 3;
-	size_t c = 1;
+	size_t c = 3;
 	size_t r = 1;
 	size_t s = 1;
 
 	tensor::Tensor<tdtype> X(1, c, h, w, 'r');
-	tensor::Tensor<tdtype> W(1, c, r, s);
 
+	cnn::convLayer cl = cnn::convLayer<tdtype>(1,c,r,s,'s', 'f', 1);
 
-	cnn::convLayer cl = cnn::convLayer<tdtype>(1,c,r,s,'s');
-
-	std::cout << "Displaying weights of conv layer :\n\n";
+	std::cout << "Displaying weights of conv layer (all channels):\n\n";
 	tdtype* weights = cl.getWeights();
 	for(auto i=0; i<r; i++){
 		for(auto j=0; j<s; j++){
@@ -26,12 +24,16 @@ int main(){
 	std::cout << "\n";
 
 	/////////////////////////////////////////////////////
-
+	X.displayShape("X");
 	std::cout << "Displaying input of conv:\n\n";
-	tdtype *xmat = X.getMatrix(0, 0);
-	for(auto i=0; i<h; i++){
-		for(auto j=0; j<w; j++){
-			std::cout << xmat[i*w + j] << " ";
+	for(auto ch=0; ch<c; ch++){
+		std::cout << "channel: "<<ch<<"\n\t";
+		tdtype *xmat = X.getMatrix(0, ch);
+		for(auto i=0; i<h; i++){
+			for(auto j=0; j<w; j++){
+				std::cout << xmat[i*w + j] << " ";
+			}
+			std::cout << "\n\t";
 		}
 		std::cout << "\n";
 	}
@@ -40,6 +42,7 @@ int main(){
 	/////////////////////////////////////////////////////
 	tensor::Tensor<tdtype> Y = cl.forward(X);
 
+	Y.displayShape("Y");
 	std::cout << "Displaying output of conv:\n\n";
 	tdtype *ymat = Y.getMatrix(0, 0);
 	for(auto i=0; i<h; i++){
